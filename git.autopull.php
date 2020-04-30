fe1bdcae2a8f3badab807574e43e7c04e34cada3
SintezCode <unarmedf@gmail.com>
2020-04-30 14:04:26 +0300

    git.autocommit.php

--- git.autopull.php
+++ git.autopull.php
@@ -5,7 +5,7 @@ if(count($included)>1)die();
 $gitpath='~/.custom';
 
 //Откатываем текущие изменения
-shell_exec('cd '.$gitpath.';git reset --hard origin/master');
+shell_exec('cd '.$gitpath.';git reset --hard HEAD');
 shell_exec('cd '.$gitpath.';git clean -ndx');
 shell_exec('cd '.$gitpath.';git clean -fdx');
 
@@ -15,7 +15,10 @@ sleep(10);
 
 //Запрашиваем статус
 $status=shell_exec('cd '.$gitpath.';git status');
-$changed=preg_match('#Your branch is behind#ui',$status);
+if(preg_match('#Your branch is ahead#ui',$status)||preg_match('#Your branch and .*? have diverged#ui',$status)){
+    shell_exec('cd '.$gitpath.';git reset --hard origin/master');
+}
+$changed=preg_match('#Your branch is behind#ui',$status)||preg_match('#Your branch and .*? have diverged#ui',$status);
 if(!$changed){
     echo 'Изменений не найдено';
     die();

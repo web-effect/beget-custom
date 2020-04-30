4aedf1702e13819cd035518a5ab51736dc096e06
SintezCode <unarmedf@gmail.com>
2020-04-30 13:49:30 +0300

    git.autocommit.php

--- git.autopull.php
+++ git.autopull.php
@@ -5,7 +5,7 @@ if(count($included)>1)die();
 $gitpath='~/.custom';
 
 //Откатываем текущие изменения
-shell_exec('cd '.$gitpath.';git reset --hard HEAD');
+shell_exec('cd '.$gitpath.';git reset --hard origin/master');
 shell_exec('cd '.$gitpath.';git clean -ndx');
 shell_exec('cd '.$gitpath.';git clean -fdx');
 
